2747bb1745e064259761066cde4039d311f7e733
PravallikaDeepu <medurimoses@gmail.com>
2025-08-22 14:31:33 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import './style.css'
 import { Link } from 'react-router-dom'
 import Axios from 'axios'
 
+
 function Navbar(props) {
   const { setSearchData } = useContext(SearchContext)
   const API_BASE_URL = process.env.REACT_BACKEND_API_URL || "http://localhost:7070"
@@ -40,7 +41,7 @@ function Navbar(props) {
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="nav" style={{ height: '80px' }}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Prana's Bookstore</a>
+        <Link className="navbar-brand" to='/'>Prana's Bookstore</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
